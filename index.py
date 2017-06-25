636cd1dfd49abb8525542b6c2a2280099415f463
System Administrator <root@Sandeeps-MacBook-Air.local>
2017-06-25 12:14:16 -0700

    parsed all the data

--- index.py
+++ index.py
@@ -1,16 +1,19 @@
 import xml.etree.ElementTree
 import urllib
+from BeautifulSoup import BeautifulSoup as soup
 
 web_site = 'http://ebace17.mapyourshow.com{0}'
 end_point = 'http://ebace17.mapyourshow.com/7_0/alphalist.cfm?alpha=*'
 file_name = 'parse/temp.html'
 
 anchors = []
-html_classes = [
-	'<p class="sc-Exhibitor_PhoneFax">',
-	'<p class="sc-Exhibitor_Address">',
-	'<div class="sc-Exhibitor_SocialMedia">',
-	'<h1 class="sc-Exhibitor_Name">']
+client_data = []
+html_classes = {
+	'phone_number' : ['sc-Exhibitor_PhoneFax','p'],
+	'address': ['sc-Exhibitor_Address','p'],
+	'social_media' : ['sc-Exhibitor_SocialMedia','div'],
+	'name': ['sc-Exhibitor_Name','h1'],
+	'link': ['sc-Exhibitor_Url','p']}
 
 
 def parse_parent_page(file_name):
@@ -34,7 +37,9 @@ def parse_child_page(web_url):
 	def child_open(web_url):
 		try:
 			data = urllib.urlopen(web_url)
-			return data
+			result = data.read()
+			data.close()
+			return result
 
 		except Exception as e:
 			print "Please check the web_url"
@@ -61,14 +66,19 @@ def parse_child_page(web_url):
 			'phone_number':None,
 			'link' : None
 		}
-		for line in data:
-			line = line.strip()
+		#t=  BeautifulSoup(html).findChildren("p", { "class" : "sc-Exhibitor_PhoneFax" })
+		page_soup = soup(data)
+		for key in client_info.keys():
+			try:
+				client_info[key] = page_soup.findAll(html_classes[key][1], {"class" : html_classes[key][0]})[0].text
+			except Exception as e:
+				print "{0} key failed for link {1}".format(key,web_url)
+		client_data.append(client_info)
 
-
-	data = child_open(web_url)
-	if not data:
+	html = child_open(web_url)
+	if not html:
 		return
-	child_write_file(data,web_url)
+	child_html_parse(html,web_url)
 
 
 
